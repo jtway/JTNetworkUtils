56f6b087965e02c80aab5083b2e394ecadb541b1
Josh Tway <twaysive@icloud.com>
2015-09-24 06:11:16 -0400

    Initial Commit

---
+++ JTNetworkUtilities/JTNetworkUtilities.h
@@ -0,0 +1,19 @@
+//
+//  JTNetworkUtilities.h
+//  JTNetworkUtilities
+//
+//  Created by Josh Tway on 9/24/15.
+//  Copyright © 2015 Josh Tway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JTNetworkUtilities.
+FOUNDATION_EXPORT double JTNetworkUtilitiesVersionNumber;
+
+//! Project version string for JTNetworkUtilities.
+FOUNDATION_EXPORT const unsigned char JTNetworkUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JTNetworkUtilities/PublicHeader.h>
+
+
