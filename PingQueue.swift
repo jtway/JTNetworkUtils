18e05a2c7e3626deadf8ed4e13146d9cf32d9187
Josh Tway <twaysive@icloud.com>
2015-10-23 07:12:29 -0400

    [WIP] This handles the simple case of pinging a host or IP with default parameters for count, size, and timeout. It also doesn't make the "pinging of it all" as streamlined as I wanted.

--- PingQueue.swift
+++ PingQueue.swift
@@ -7,3 +7,55 @@
 //
 
 import Foundation
+
+public class PingQueue {
+
+    public typealias PingOnceCompletionHandler = (response: PingResponse) -> Void
+    public typealias PingQueueCompletionHandler = (Void) -> Void
+
+    private(set) var dispatchQueue: dispatch_queue_t
+
+    private let dispatchGroup: dispatch_group_t = dispatch_group_create()
+
+    var pings: [Ping] = [Ping]()
+
+    init() {
+        let queueAttributes = dispatch_queue_attr_make_with_qos_class(DISPATCH_QUEUE_CONCURRENT, QOS_CLASS_UTILITY, 0)
+        dispatchQueue = dispatch_queue_create("com.jtway.PingQueue", queueAttributes)
+    }
+
+    func pingOnce(hostname: String, completionHandler: PingOnceCompletionHandler) {
+        let ping: Ping = Ping(hostname: hostname)
+        ping.dispatchQueue = dispatchQueue
+        ping.completionHandler = { pingResponses in
+            if pingResponses.count > 1 {
+                // @TODO Add real error handling
+                print("Unexpected number of ping responses")
+            }
+
+            if pingResponses.count == 0 {
+                print("No pings!")
+            }
+
+            completionHandler(response: pingResponses.first!)
+
+            // Remove from the array of pings. Hopefully this doesn't cause a strong retain cycle and once
+            //   we leave the completionHandler we'll delete the Ping object
+            if let pingIndex = self.pings.indexOf(ping) {
+                self.pings.removeAtIndex(pingIndex)
+            }
+
+            dispatch_group_leave(self.dispatchGroup)
+        }
+
+        pings.append(ping)
+
+        dispatch_group_enter(dispatchGroup)
+        ping.start()
+    }
+
+    func waitForAllPingsToComplete() {
+        // If a ping doesn't get a reply this will wait forever, which isn't really what I want
+        dispatch_group_wait(dispatchGroup, DISPATCH_TIME_FOREVER)
+    }
+}
